14afde4779d00bf1f3663e2597e273a30456b936
subhabrata <subhabratap733@gmail.com>
2024-12-13 16:17:29 +0530

    bubble search added

---
+++ CORE-JAVA/SORTING_SEARCHING/src/com/sp/arraysearch/BubbleSearch.java
@@ -0,0 +1,23 @@
+package com.sp.arraysearch;
+
+import java.util.Scanner;
+
+public class BubbleSearch {
+	public static void main(String[] args) {
+		int a[]= {2,1,4,3,1,7,8,0,5,4};
+		Scanner sc=new Scanner(System.in);
+		int index=-1;
+		System.out.println("Enter the element to be searched");
+		int key=sc.nextInt();
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==key)
+			{
+				index=i;
+				break;
+			}
+		}
+		System.out.println("Element found at index : "+index);
+		sc.close();
+	}
+}
